965a6281c21eec7ce46313b6f3ed7fbc52cb5e77
quyet7043h-cyber <quyet7043h@gmail.com>
2026-08-23 19:34:48 +0700

    Update database.types.ts

--- src/lib/database.types.ts
+++ src/lib/database.types.ts
@@ -2748,11 +2748,11 @@ export type Database = {
     }
     Functions: {
       join_clan_by_share_token: {
-         Args: {
-           p_token: string
+        Args: {
+          p_token: string
          }
          Returns: Json
-     }
+      }
       _inlaw_person_card: {
         Args: {
           hide_living: boolean
